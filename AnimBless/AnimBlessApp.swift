300abe22df3a200a554b0e5c6312f32b3d6194e8
iblessme <vldikk12@gmail.com>
2022-05-05 14:57:22 +0300

    Initial Commit

---
+++ AnimBless/AnimBlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimBlessApp.swift
+//  AnimBless
+//
+//  Created by iBlessme on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimBlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
